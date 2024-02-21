c4476a15db506e7004fc138caea76051bab9d25e
janmoser87 <m.janmoser@gmail.com>
2024-02-21 17:47:14 +0100

    example demo

--- app/page.js
+++ app/page.js
@@ -1,95 +1,60 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { Button, Modal, ModalDialog, ModalClose, Typography } from "@mui/joy";
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [showModal, setShowModal] = useState(false);
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	/**
+   * I want this modal to be positioned 10px from the top border of the screen.
+   * As you can see, <ModalDialog sx={{top: "10px"}}> is not the solution,
+   * the modal partially overflows the screen.
+   * 
+   * Desired behavior:
+   * 
+   * /---------------/
+   * /     _____     /
+   * /    |     |    /
+   * /    |_____|    /
+   * /               /
+   * /               /
+   * /                /
+   * /                /
+   * /--------------- /
+  
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+   * Actuall behavior:
+   * 
+   * /---------------/
+   * /    |_____|    /
+   * /               /
+   * /               /
+   * /                /
+   * /                /
+   * /--------------- /
+  */
+	const myModal = (
+		<Modal open={showModal} onClose={() => setShowModal(false)}>
+			<ModalDialog sx={{ top: "10px" }}>
+				<ModalClose />
+				<Typography>
+					Modal content<br />
+          Modal content<br />
+          Modal content<br />
+          Modal content<br />
+          Modal content<br />
+          Modal content<br />
+				</Typography>
+			</ModalDialog>
+		</Modal>
+	);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<div>
+			<Button onClick={() => setShowModal(true)}>Open modal</Button>
+			{showModal && myModal}
+		</div>
+	);
 }
